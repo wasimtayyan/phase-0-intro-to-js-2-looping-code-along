7c0599e9d9a89a03d5af72bf1d69d5182e40dac4
wasim altayyan <wasimtayyan@gmail.com>
2023-04-06 15:51:21 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+ function writeCards(array , event){
+    const message = []
+    for(let i = 0 ; i < array.length; i++){
+        message.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);   
+    }
+    return message;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+function countDown(numder){
+    while(numder >= 0){
+        console.log(numder);
+        numder--;
+    }
+}
+countDown(10)
